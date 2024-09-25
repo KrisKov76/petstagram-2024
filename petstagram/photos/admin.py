ce0fe7ad4a2cc7e7bdeebe3b5bfb1c798d6cad53
Kristian Kovachev <123325623+KrisKov76@users.noreply.github.com>
2024-09-26 00:04:15 +0300

    feat: added comment and like models

--- petstagram/photos/admin.py
+++ petstagram/photos/admin.py
@@ -5,7 +5,9 @@ from petstagram.photos.models import Photo
 
 @admin.register(Photo)
 class Admin(admin.ModelAdmin):
-    pass
-    
+    list_display = ('id', 'date_of_publication', 'description', 'get_tagged_pets')
 
+    @staticmethod
+    def get_tagged_pets(obj):
+        return ', '.join(str(pet) for pet in obj.tagged_pets.all())
 # Register your models here.
