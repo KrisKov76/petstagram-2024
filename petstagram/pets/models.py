e87ccb880ff7015d51ce6d599b3bf69c44aece74
Kristian Kovachev <123325623+KrisKov76@users.noreply.github.com>
2024-09-25 20:47:55 +0300

    fix: fixed slug field

--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -6,9 +6,10 @@ class Pet(models.Model):
     name = models.CharField(max_length=30)
     personal_photo = models.URLField()
     date_of_birth = models.DateField(blank=True, null=True)
-    slug = models.SlugField(null=False, blank=True, unique=True)
+    slug = models.SlugField(null=True, blank=True, unique=True, editable=False)
 
     def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         if not self.slug:
             self.slug = slugify(f"{self.name}-{self.id}")
         super().save(*args, **kwargs)  # запазва обекта в базата, без super().save не се запазва в базата
